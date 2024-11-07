9dc30bdbf13d6da7d2de650a51cdfa09fcf9acb5
prashanth770 <jkklprashanth@gmail.com>
2024-11-07 22:42:37 +0530

    file2 commited

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        Scanner t=new Scanner(System.in);
+        int n=t.nextInt();
+        if(n%2==0){
+            System.out.println("even number");
+        }
+        else{
+            System.out.println("odd number");
+           
+        }
+    }
+}
